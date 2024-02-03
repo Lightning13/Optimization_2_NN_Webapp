c77e31be439f1e1862057b4b7add0d4b32e2ec21
abhijitanil@utexas.edu <abhijitanil@utexas.edu>
2024-02-03 03:41:55 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -18,15 +18,15 @@ class Form1(Form1Template):
     elif file.name.endswith('.csv'):
         self.text_test.text = "CSV file uploaded. Processing..."
 
-        pixel_intensities = anvil.server.call('process_csv', file)
-        # Convert the pixel intensities to base64-encoded image data
-        image_data = anvil.server.call('create_image_data',pixel_intensities)
+        # pixel_intensities = anvil.server.call('process_csv', file)
+        # # Convert the pixel intensities to base64-encoded image data
+        # image_data = anvil.server.call('create_image_data',pixel_intensities)
 
-        # Set the source of the image component to the base64-encoded image data
-        # self.image_1.source = anvil.BlobMedia(content=bytes(image_data, 'utf-8'), content_type='image/png')
+        # # Set the source of the image component to the base64-encoded image data
+        # # self.image_1.source = anvil.BlobMedia(content=bytes(image_data, 'utf-8'), content_type='image/png')
 
-        self.image_1.source = 'https://drive.google.com/file/d/1UkH_oeokSDxOd2D5Xx8rV2c5hpyLiKxt/view?usp=sharing'
-        anvil.server.call('save_uploaded_csv', file)
+        # self.image_1.source = 'https://drive.google.com/file/d/1UkH_oeokSDxOd2D5Xx8rV2c5hpyLiKxt/view?usp=sharing'
+        self.image_1.source = anvil.server.call('process_csv', file)
         self.text_test.text = "Processed"
     else:
         self.text_test.text = "Please upload a CSV file"
