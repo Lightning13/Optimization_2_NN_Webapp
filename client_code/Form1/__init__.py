35a15f9341df816b36ba374fce9227854724f5bb
abhijitanil@utexas.edu <abhijitanil@utexas.edu>
2024-02-03 00:31:59 +0000

    Created ServerModule1; Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,6 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.server
 
 class Form1(Form1Template):
   def __init__(self, **properties):
@@ -7,3 +8,19 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def uploader_change(self, file, **event_args):
+    """This method is called when a new file is loaded into this FileLoader"""
+    if file is None:
+        self.text_test.text = "No file uploaded"
+    elif file.name.endswith('.csv'):
+        self.df = anvil.server.call('import_csv_data', file)
+        self.text_test.text = self.df[0]
+    else:
+        self.text_test.text = "Please upload a CSV file"
+    pass
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    self.text_test.text = anvil.server.call(str('sum_1'))
+    pass
