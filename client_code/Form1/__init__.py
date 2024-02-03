3f7420ce20b7362a4dbd49ad467cb7042ebc90cc
abhijitanil@utexas.edu <abhijitanil@utexas.edu>
2024-02-03 01:13:06 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,6 +1,7 @@
 from ._anvil_designer import Form1Template
 from anvil import *
 import anvil.server
+import base64
 
 class Form1(Form1Template):
   def __init__(self, **properties):
@@ -10,15 +11,25 @@ class Form1(Form1Template):
     # Any code you write here will run before the form opens.
 
   def uploader_change(self, file, **event_args):
-    """This method is called when a new file is loaded into this FileLoader"""
-    if file is None:
-        self.text_test.text = "No file uploaded"
-    elif file.name.endswith('.csv'):
-        self.df = anvil.server.call('import_csv_data', file)
-        self.text_test.text = self.df[0]
-    else:
-        self.text_test.text = "Please upload a CSV file"
-    pass
+        """This method is called when a new file is loaded into this FileLoader"""
+        if file is None:
+            self.text_test.text = "No file uploaded"
+        elif file.name.endswith('.csv'):
+            self.df = anvil.server.call('import_csv_data', file)
+            
+            img_bytes = anvil.server.call('classify_image', file)
+    
+            # Check if img_bytes is already bytes-like
+            if isinstance(img_bytes, bytes):
+                # Display the image on the webpage
+                image_source = 'data:image/png;base64,' + base64.b64encode(img_bytes).decode('utf-8')
+                self.image_1.source = image_source  # Assuming 'image_1' is the name of your Image component
+            else:
+                self.text_test.text = "Error: Invalid image bytes"
+        else:
+            self.text_test.text = "Please upload a CSV file"
+          
+  pass      
 
   def button_1_click(self, **event_args):
     """This method is called when the button is clicked"""
