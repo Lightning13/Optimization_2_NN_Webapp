ded1662d53f6c680403caed3d4e551ea8992a159
abhijitanil@utexas.edu <abhijitanil@utexas.edu>
2024-02-05 03:34:15 +0000

    Created Form4; Edited settings and Form1

---
+++ client_code/Form4/__init__.py
@@ -0,0 +1,27 @@
+from ._anvil_designer import Form4Template
+from anvil import *
+import anvil.server
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+class Form4(Form4Template):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
+    anvil.users.login_with_form()
+
+  def link_1_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form("Form1")
+    pass
+
+  def link_2_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form("Form2")
+    pass
